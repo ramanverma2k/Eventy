619051ff7dfc2611cd1a6f6655dbcd4d1de3a8bf
ramanverma2k <ramanverma4183@gmail.com>
2022-04-15 13:11:58 +0530

    eventy_organizer: implement bloc observer to monitor state changes.

--- packages/eventy_organizer/lib/main.dart
+++ packages/eventy_organizer/lib/main.dart
@@ -1,10 +1,24 @@
+import 'dart:async';
+import 'dart:developer' as developer;
+
 import 'package:eventy_db/eventy_db.dart';
 import 'package:eventy_organizer/login/login.dart';
+import 'package:eventy_organizer/observer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 void main() {
-  runApp(const MyApp());
+  runZonedGuarded(
+    () async {
+      await BlocOverrides.runZoned(
+        () async => runApp(const MyApp()),
+        blocObserver: MyAppBlocObserver(),
+      );
+    },
+    (error, stackTrace) =>
+        developer.log(error.toString(), stackTrace: stackTrace),
+  );
 }
 
 class MyApp extends StatelessWidget {
