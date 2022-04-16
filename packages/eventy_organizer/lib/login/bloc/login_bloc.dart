ccc059077bc1069bc44d4dbbe6ce35ab7336213c
ramanverma2k <ramanverma4183@gmail.com>
2022-04-16 10:43:38 +0530

    eventy_organizer: register user model as an instance to keep track of login state.

--- packages/eventy_organizer/lib/login/bloc/login_bloc.dart
+++ packages/eventy_organizer/lib/login/bloc/login_bloc.dart
@@ -1,6 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:eventy_db/queries/queries.dart';
+import 'package:eventy_organizer/models/user_model.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:get_it/get_it.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:meta/meta.dart';
 
@@ -11,26 +13,30 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
   final GraphQLClient client;
 
   LoginBloc({required this.client}) : super(LoginInitial()) {
-    on<LoginEventStarted>((event, emit) async {
-      final QueryOptions options =
-          QueryOptions(document: gql(Queries.getUser), variables: {
-        "email": event.username,
-        "password": event.password,
-      });
+    on<LoginEventStarted>(
+      (event, emit) async {
+        final QueryOptions options =
+            QueryOptions(document: gql(Queries.getUser), variables: {
+          "email": event.username,
+          "password": event.password,
+        });
 
-      emit(LoginInProgress());
-      try {
-        final result = await client.query(options);
+        emit(LoginInProgress());
+        try {
+          final result = await client.query(options);
 
-        if (result.data!["exception"] == null &&
-            result.data!["users"].isNotEmpty) {
-          emit(LoginSuccess());
-        } else {
-          emit(LoginInitial());
+          if (result.data!["exception"] == null &&
+              result.data!["users"].isNotEmpty) {
+            GetIt.I.registerSingleton(User.fromJson(result.data!["users"][0]),
+                instanceName: "user");
+            emit(LoginSuccess());
+          } else {
+            emit(LoginInitial());
+          }
+        } catch (e) {
+          emit(LoginFailed());
         }
-      } catch (e) {
-        emit(LoginFailed());
-      }
-    });
+      },
+    );
   }
 }
--- packages/eventy_organizer/lib/signup/bloc/signup_bloc.dart
+++ packages/eventy_organizer/lib/signup/bloc/signup_bloc.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:eventy_db/mutations/mutations.dart';
+import 'package:eventy_organizer/models/user_model.dart';
+import 'package:get_it/get_it.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:meta/meta.dart';
 import 'package:uuid/uuid.dart';
@@ -11,35 +13,39 @@ class SignupBloc extends Bloc<SignupEvent, SignupState> {
   final GraphQLClient client;
 
   SignupBloc({required this.client}) : super(SignupInitial()) {
-    on<SignupEventStarted>((event, emit) async {
-      final _id = const Uuid().v4();
+    on<SignupEventStarted>(
+      (event, emit) async {
+        final _id = const Uuid().v4();
 
-      final MutationOptions options = MutationOptions(
-          document: gql(Mutations.registerAdminUser),
-          variables: {
-            "id": _id,
-            "username": event.username,
-            "password": event.password,
-            "first_name": event.first_name,
-            "last_name": event.last_name,
-            "email": event.email,
-            "contact_no": event.contact_no,
-            "description": event.description,
-            "is_admin": true,
-          });
+        final MutationOptions options = MutationOptions(
+            document: gql(Mutations.registerAdminUser),
+            variables: {
+              "id": _id,
+              "username": event.username,
+              "password": event.password,
+              "first_name": event.first_name,
+              "last_name": event.last_name,
+              "email": event.email,
+              "contact_no": event.contact_no,
+              "description": event.description,
+              "is_admin": true,
+            });
 
-      emit(SignupInProgress());
-      try {
-        final result = await client.mutate(options);
+        emit(SignupInProgress());
+        try {
+          final result = await client.mutate(options);
 
-        if (result.data!["action"]["id"].isNotEmpty) {
-          emit(SignupSuccess());
-        } else {
-          emit(SignupInitial());
+          if (result.data!["action"]["id"].isNotEmpty) {
+            GetIt.I.registerSingleton(User.fromJson(result.data!["action"]),
+                instanceName: "user");
+            emit(SignupSuccess());
+          } else {
+            emit(SignupInitial());
+          }
+        } catch (e) {
+          emit(SignupFailed());
         }
-      } catch (e) {
-        emit(SignupFailed());
-      }
-    });
+      },
+    );
   }
 }
