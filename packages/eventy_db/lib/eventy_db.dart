933de09ac6c6d3ed542bf321dbe1b8cb90260b05
ramanverma2k <ramanverma4183@gmail.com>
2022-04-18 18:52:16 +0530

    eventy_db: add local network ip for quick access to database from another computer.

--- packages/eventy_db/lib/eventy_db.dart
+++ packages/eventy_db/lib/eventy_db.dart
@@ -6,6 +6,9 @@ import 'package:graphql_flutter/graphql_flutter.dart';
 class DatabaseConfig {
   static final HttpLink httpLink = HttpLink("http://localhost:8080/v1/graphql");
 
+  // Url to connect from a different computer on local network
+  // static final HttpLink httpLink = HttpLink("http://192.168.1.16:8080/v1/graphql");
+
   static final ValueNotifier<GraphQLClient> client = ValueNotifier(
     GraphQLClient(
       link: httpLink,
