bf671eade1701dd3f83916034a42742a45749d6e
ramanverma2k <ramanverma4183@gmail.com>
2022-04-25 23:34:36 +0530

    recreate common package and add database in pubspec

---
+++ packages/common/android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.common
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- packages/common/lib/common.dart
+++
@@ -1,3 +0,0 @@
-library common;
-
-export 'src/common.dart';
