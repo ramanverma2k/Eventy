8dd2ddbe23f24e03940e3a1a5aa4fb5aaa92de0f
ramanverma2k <ramanverma4183@gmail.com>
2022-05-08 00:41:55 +0530

    database: setup client and code_gen

--- packages/database/lib/src/database.dart
+++ packages/database/lib/src/database.dart
@@ -1,7 +1,16 @@
+import 'package:graphql/client.dart';
+
 /// {@template database}
 /// Database package for communicating with backend
+/// ---
 /// {@endtemplate}
 class Database {
   /// {@macro database}
-  const Database();
+  /// Creates a GraphQL client using [HttpLink] and [GraphQLCache].
+  static final GraphQLClient client = GraphQLClient(
+    /// [HttpLink] is used in [GraphQLClient] to communicate with GraphQL server
+    link: HttpLink('http://localhost:8080/v1/graphql'),
+    // The default store is the InMemoryStore, which does NOT persist to disk
+    cache: GraphQLCache(),
+  );
 }
