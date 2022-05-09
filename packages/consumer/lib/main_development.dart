a793a632c5726a99bc8530a153400b91d1292be1
ramanverma2k <ramanverma4183@gmail.com>
2022-05-09 23:35:28 +0530

    consumer: setup base with Repositories

--- packages/consumer/lib/main_development.dart
+++ packages/consumer/lib/main_development.dart
@@ -5,9 +5,26 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+import 'package:database/database.dart';
 import 'package:eventy_consumer/app/app.dart';
 import 'package:eventy_consumer/bootstrap.dart';
+import 'package:flutter/widgets.dart';
+import 'package:local_storage_api/local_storage_api.dart';
 
 void main() {
-  bootstrap(() => const App());
+  bootstrap(() async {
+    final _sharedPreferences = await SharedPreferences.getInstance();
+
+    final _client = GraphQLClient(
+      link: HttpLink('http://localhost:8080/v1/graphql'),
+      cache: GraphQLCache(),
+    );
+
+    WidgetsFlutterBinding.ensureInitialized();
+
+    return App(
+      sharedPreferences: _sharedPreferences,
+      graphqlClient: _client,
+    );
+  });
 }
--- packages/consumer/test/app/view/app_test.dart
+++ packages/consumer/test/app/view/app_test.dart
@@ -5,14 +5,33 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+import 'package:database/database.dart';
 import 'package:eventy_consumer/app/app.dart';
 import 'package:eventy_consumer/counter/counter.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:local_storage_api/local_storage_api.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockSharedPreferences extends Mock implements SharedPreferences {}
+
+class MockGraphQLClient extends Mock implements GraphQLClient {}
 
 void main() {
+  late SharedPreferences sharedPreferences;
+  late GraphQLClient graphqlClient;
+
+  setUp(() {
+    sharedPreferences = MockSharedPreferences();
+    graphqlClient = MockGraphQLClient();
+  });
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(const App());
+      await tester.pumpWidget(
+        App(
+          sharedPreferences: sharedPreferences,
+          graphqlClient: graphqlClient,
+        ),
+      );
       expect(find.byType(CounterPage), findsOneWidget);
     });
   });
